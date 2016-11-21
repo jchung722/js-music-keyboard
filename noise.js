0f4385d52083b704e9cb7508c76a30106124ce9c
Jessica <jchung722@gmail.com>
2016-11-21 13:54:49 -0800

    key press and basic requirements met

--- noise.js
+++ noise.js
@@ -8,5 +8,29 @@ $(document).ready( function() {
     // }
   });
 
+  $("body").keydown(function(event) {
+    var note = '';
+    if (event.key == 'c') {
+      note = document.getElementById('cAudio');
+    } else if (event.key == 'd'){
+      note = document.getElementById('dAudio');
+    } else if (event.key == 'e'){
+      note = document.getElementById('eAudio');
+    } else if (event.key == 'f'){
+      note = document.getElementById('fAudio');
+    } else if (event.key == 'g'){
+      note = document.getElementById('gAudio');
+    } else if (event.key == 'a'){
+      note = document.getElementById('aAudio');
+    } else if (event.key == 'b'){
+      note = document.getElementById('bAudio');
+    }
+    // if (note.paused) {
+    note.play();
+    // } else {
+    note.currentTime = 0;
+    // }
+  });
+
 
 });
