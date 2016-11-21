cbaa8100c94a8320193cd970516d86eb519ae967
Jessica <jchung722@gmail.com>
2016-11-21 13:43:31 -0800

    click notes working

--- noise.js
+++ noise.js
@@ -1,3 +1,12 @@
 $(document).ready( function() {
-  // your code here
+  $('.instrument').on('click', 'button', function(event) {
+    var note = document.getElementById($(this).html() + 'Audio');
+    // if (note.paused) {
+    note.play();
+    // } else {
+    note.currentTime = 0;
+    // }
+  });
+
+
 });
