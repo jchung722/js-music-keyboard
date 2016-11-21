a9945a467ce09c55b169f2c05ed96d731cce2f75
Jessica <jchung722@gmail.com>
2016-11-21 14:29:24 -0800

    activates css when key is pressed and let go

--- noise.js
+++ noise.js
@@ -8,22 +8,29 @@ $(document).ready( function() {
     // }
   });
 
-  $("body").keydown(function(event) {
+  $("body").keypress(function(event) {
     var note = '';
     if (event.key == 'c') {
       note = document.getElementById('cAudio');
+      $('.c').addClass('active');
     } else if (event.key == 'd'){
       note = document.getElementById('dAudio');
+      $('.d').addClass('active');
     } else if (event.key == 'e'){
       note = document.getElementById('eAudio');
+      $('.e').addClass('active');
     } else if (event.key == 'f'){
       note = document.getElementById('fAudio');
+      $('.f').addClass('active');
     } else if (event.key == 'g'){
       note = document.getElementById('gAudio');
+      $('.g').addClass('active');
     } else if (event.key == 'a'){
       note = document.getElementById('aAudio');
+      $('.a').addClass('active');
     } else if (event.key == 'b'){
       note = document.getElementById('bAudio');
+      $('.b').addClass('active');
     }
     // if (note.paused) {
     note.play();
@@ -31,6 +38,25 @@ $(document).ready( function() {
     note.currentTime = 0;
     // }
   });
+
+  $("body").keyup(function(event) {
+    if (event.key == 'c') {
+      $('.c').removeClass('active');
+    } else if (event.key == 'd') {
+      $('.d').removeClass('active');
+    } else if (event.key == 'e') {
+      $('.e').removeClass('active');
+    } else if (event.key == 'f') {
+      $('.f').removeClass('active');
+    } else if (event.key == 'g') {
+      $('.g').removeClass('active');
+    } else if (event.key == 'a') {
+      $('.a').removeClass('active');
+    } else if (event.key == 'b') {
+      $('.b').removeClass('active');
+    }
+  });
+
 
 
 });
